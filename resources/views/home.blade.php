ea6a8efde31de555f950241d9ad41a046feac040
root <root@ip-172-31-42-20.eu-north-1.compute.internal>
2026-05-31 09:24:04 +0000

    Fix home page placeholder

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
-[Paste the complete code block provided above here]
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Academy Project</title>
+</head>
+<body>
+    <h1>Laravel Application Working Successfully</h1>
+</body>
+</html>
